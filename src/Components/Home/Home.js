8ae2fa6c5e66e7e4f183b83ab061da451722fe57
andrescuello7 <andrescuellotrabajo@gmail.com>
2021-02-17 22:44:04 -0300

    Api de prueba

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,36 @@
+import Axios from "axios"
+import { useEffect, useState, UseState } from "react"
+
 const Home = () => {
+  const [ input, setInput ] = useState("")
+  const [ temperatura, setTemperatura ] = useState("")
+  const promedio = parseInt(23)
+
+  useEffect(() => {
+    ApiClima()
+    Final()
+  }, [])
+
+  const ApiClima = async () => {
+    try{
+      const traer = await Axios.get(`http://api.weatherstack.com/current?access_key=cb7b3da9b1bbc62baf2d7fb20db82de2&query=Tucuman`)
+      setInput(traer.data.current.temperature)
+    }catch (error){
+      console.log(error)
+    }
+  }
+
+  const Final = () =>{
+    if (input < promedio) {
+      setTemperatura('Frio')
+    } else {
+      setTemperatura('Calor')
+    }
+  }
+
   return (
     <div>
-      Hola capo
+      <h1>{temperatura}</h1>
     </div>
   );
 }
