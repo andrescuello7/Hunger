fb2146fef09804b76f44f053c23a33a72a259cbb
andrescuello7 <andrescuellotrabajo@gmail.com>
2021-02-18 00:32:32 -0300

    actualizacion

--- src/Components/Agregar/Agregar.js
+++ src/Components/Agregar/Agregar.js
@@ -1,16 +1,33 @@
-import { Button, Modal, Form, InputGroup, Dropdown, FormControl, DropdownButton } from "react-bootstrap"
+import { Button, Modal, Form, InputGroup, Dropdown, DropdownButton } from "react-bootstrap"
 import { useState } from "react"
+import { getStorageArray, setStorage } from "../../Utils/UtilsStorage"
 
 const Agregar = () => {
+    //UseState de Modal
     const [show, setShow] = useState(false);
-
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    //UseState de Inputs
+    const [ texto, setTexto ] = useState({ comida: " ", dia: " ", temperatura: " " })
 
+    //Funciones
+    const ButtonOnSubmit = (e) =>{
+        e.preventDefault();
+        e.stopPropagation();
+        const cuentas = getStorageArray("comida");
+        const cuentasUp = [...cuentas, texto];
+        setStorage("comida", cuentasUp);
+    }
+    const OnChange = (e) =>{
+        const value = e.target.value;
+        const name = e.target["name"];
+        const objeto = { ...texto, [name]: value };
+        setTexto(objeto);
+    }
     return (
         <>
             <Button variant="primary" onClick={handleShow}>
-                Launch demo modal
+                Agregar Comida
             </Button>
 
             <Modal show={show} onHide={handleClose}>
@@ -18,38 +35,30 @@ const Agregar = () => {
                     <Modal.Title>Agregar Una Comida</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                <Form>
+                <Form onSubmit={ButtonOnSubmit}>
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Agregar Comida</Form.Label>
-                        <Form.Control type="email" placeholder="Ingrese Comida" />
+                        <Form.Control type="text" name="comida" onChange={OnChange} placeholder="Ingrese Comida" />
                     </Form.Group>
                     <Form className="Opciones">
                         <Form.Group controlId="formBasicEmail">
-                                <DropdownButton
-                                as={InputGroup.Prepend}
-                                variant="outline-secondary"
-                                title="Dia de Semana"
-                                id="input-group-dropdown-1"
-                                >
-                                <Dropdown.Item href="#">Lunes</Dropdown.Item>
-                                <Dropdown.Item href="#">Martes</Dropdown.Item>
-                                <Dropdown.Item href="#">Miercoles</Dropdown.Item>
-                                <Dropdown.Item href="#">Jueves</Dropdown.Item>
-                                <Dropdown.Item href="#">Viernes</Dropdown.Item>
-                                <Dropdown.Item href="#">Sabado</Dropdown.Item>
-                                <Dropdown.Item href="#">Domingo</Dropdown.Item>
-                                </DropdownButton>
+                            <select class="form-control"  name="dia" onChange={OnChange} required>
+                                <option disabled selected>Para que dia?</option>
+                                <option>Lunes</option>
+                                <option>Martes</option>
+                                <option>Miercoles</option>
+                                <option>Jueves</option>
+                                <option>Viernes</option>
+                                <option>Sabado</option>
+                                <option>Domingo</option>
+                            </select>
                         </Form.Group>
                         <Form.Group controlId="formBasicEmail">
-                                <DropdownButton
-                                as={InputGroup.Prepend}
-                                variant="outline-secondary"
-                                title="Dia o Noche"
-                                id="input-group-dropdown-1"
-                                >
-                                <Dropdown.Item href="#">Dia</Dropdown.Item>
-                                <Dropdown.Item href="#">Noche</Dropdown.Item>
-                                </DropdownButton>
+                        <select class="form-control" name="temperatura" onChange={OnChange} required>
+                                <option disabled selected>Cuando hace Calor o Frio?</option>
+                                <option>Frio</option>
+                                <option>Calor</option>
+                            </select>
                         </Form.Group>
                     </Form>
                     <Button variant="primary" type="submit">
